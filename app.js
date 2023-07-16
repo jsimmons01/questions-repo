edc644311d924bd7385ea737903653fd2bda4e3e
Jahmella <81432170+jsimmons01@users.noreply.github.com>
2023-07-15 23:36:45 -0400

    Creates eventlistener for buttons

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //using selectors inside the element
 // traversing the dom
+
+const questionBtn = document.querySelectorAll(".question-btn);
+
+questionBtn.forEach(function(btn){
+
+  btn.addEventListener("click", (e)=> {
+
+    
+    
+  })
+  
+})
+
+                                        
