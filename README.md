372c3054a826546e2896afd23e281fc4ec73ad8e
Jahmella Williams <simmons.jahmella@gmail.com>
2023-07-16 23:42:04 -0400

    Toggles between hide text and show text

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Creating a FAQS page. I will have to traversing the DOM and using selectors.
 
 ### What did I learn?
 
+I learned how to toggle between showing text and two different buttons using traversing. Get the parent element to toggle betwen the sections
+
 ### What could I do differently?
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //using selectors inside the element
 // traversing the dom
 
-const questionBtn = document.querySelectorAll(".question-btn);
+const questionBtn = document.querySelectorAll(".question-btn");
 
 questionBtn.forEach(function(btn){
 
   btn.addEventListener("click", (e)=> {
+    const question = e.currentTarget.parentElement.parentElement
 
+    question.classList.toggle("show-text")
+  
     
+   
     
   })
   
